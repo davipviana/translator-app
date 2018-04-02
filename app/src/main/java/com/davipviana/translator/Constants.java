ab836b21f9bf20a41b8ce5b82758f9eaa5372242
Davi Viana <davipviana@gmail.com>
2018-04-02 12:05:30 -0300

    Addind constants class.

---
+++ app/src/main/java/com/davipviana/translator/Constants.java
@@ -0,0 +1,12 @@
+package com.davipviana.translator;
+
+public class Constants {
+    public static final String PRIMARY_SUBSCRIPTION_KEY = "TBA";
+    public static final String SECONDARY_SUBSCRIPTION_KEY = "TBA";
+
+    public static final String[] LANGUAGE_CODES = {"en-us", "en-gb", "fr-fr", "it-it", "zh-cn", "es-es"};
+
+    public static final String SPEECH_TO_TEXT_PREFERENCES = "SpeechToTextPreferences";
+    public static final String SPEECH_MODE_INDEX = "SpeechModeIndex";
+    public static final String BASE_LANGUAGE_INDEX = "BaseLanguageIndex";
+}
