d2c2d87e47c84b7dac33844105866b35cdb5bbb6
Davi Viana <davipviana@gmail.com>
2018-04-02 19:05:32 -0300

    Main activity spinners initialization.

--- app/src/main/java/com/davipviana/translator/MainActivity.java
+++ app/src/main/java/com/davipviana/translator/MainActivity.java
@@ -1,21 +1,27 @@
 package com.davipviana.translator;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.microsoft.cognitiveservices.speechrecognition.ISpeechRecognitionServerEvents;
 import com.microsoft.cognitiveservices.speechrecognition.MicrophoneRecognitionClient;
 import com.microsoft.cognitiveservices.speechrecognition.SpeechRecognitionMode;
 
 public class MainActivity extends AppCompatActivity
-    implements ISpeechRecognitionServerEvents {
+        /*implements ISpeechRecognitionServerEvents*/ {
 
     public static final String LOG_TAG = "TranslatorApp";
     private MicrophoneRecognitionClient micClient = null;
@@ -40,35 +46,124 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowHomeEnabled(false);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        resultText = (TextView) findViewById(R.id.resultText);
+        onlineIcon = getResources().getIdentifier("@android:drawable/presence_audio_online", null, null);
+        busyIcon = getResources().getIdentifier("@android:drawable/ic_voice_search", null, null);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (hasInternetConnection()) {
+                    resultText.setText("");
+                    //initRecording();
+                    if (micClient != null) {
+                        if (speechMode.equals(SpeechRecognitionMode.ShortPhrase)) {
+                            if (!hasStartedRecording) {
+                                micClient.startMicAndRecognition();
+                            }
+                        } else {
+                            if (!hasStartedRecording) {
+                                micClient.startMicAndRecognition();
+                            } else {
+                                micClient.endMicAndRecognition();
+                            }
+                        }
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Please check your internet connection", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        initLanguageSpinner();
+        initSpeechModeSpinner();
+    }
+
+    private boolean hasInternetConnection() {
+        ConnectivityManager connectivityManager = ((ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager != null
+                && connectivityManager.getActiveNetworkInfo() != null
+                && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
+    private void initLanguageSpinner() {
+        final Spinner spinner = (Spinner) findViewById(R.id.language_spinner);
+        spinner.setSaveEnabled(true);
+        // TODO spinner.setSelection(SharedPreferencesUtils.getBaseLanguageIndex(this));
+        // TODO languageCode = Constants.LANGUAGE_CODES[SharedPreferencesUtils.getBaseLanguageIndex(this));
+
+        spinner.post(new Runnable() {
+            @Override
+            public void run() {
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        Log.d(LOG_TAG, "In Language Spinner onItemSelected");
+                        languageCode = Constants.LANGUAGE_CODES[position];
+                        hasOptionChanged = true;
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        // no action required, but must implement this method
+                    }
+                });
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initSpeechModeSpinner() {
+        final Spinner spinner = (Spinner) findViewById(R.id.speech_mode_spinner);
+        spinner.setSaveEnabled(true);
+        // TODO int pref = SharedPreferencesUtils.getSpeechModeIndex(this);
+        // TODO spinner.setSelection(pref);
+        // TODO speechMode = pref == 0 ? SpeechRecognitionMode.ShortPhrase : SpeechRecognitionMode.LongDictation;
+        spinner.post(new Runnable() {
+            @Override
+            public void run() {
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        Log.d(LOG_TAG, "In Speech Mode onItemSelected");
+                        speechMode = position == 0 ? SpeechRecognitionMode.ShortPhrase : SpeechRecognitionMode.LongDictation;
+                        hasOptionChanged = true;
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        // no action required, but must implement this method
+                    }
+                });
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
